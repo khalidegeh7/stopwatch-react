2a10deddb261a2495fcdafd289dde6bc2de47ce2
khalid <khalidegeh123@gmail.com>
2025-10-08 00:53:10 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Counter from './components/Counter.jsx'
+import Stopwatch from './components/Stopwatch.jsx'
+
+
+const App = () => {
+  return (
+    <>
+      
+      <Counter/>
+      <Stopwatch/>
+    
+      
+
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Counter.jsx
@@ -0,0 +1,31 @@
+import React, {useState} from "react";
+
+function Counter() {
+
+//     const [count, SetCount] = useState(0)
+
+//     const increment = () => {
+//         SetCount(count + 1)
+//     }
+
+//     const decrement = () => {
+//         SetCount(count - 1)
+//     }
+
+//     const reset = () => {
+//         SetCount(count * 0)
+//     }
+
+//     return(
+//         <div>
+//             <h1>Count: {count}</h1>
+//             <button onClick={increment}>Increase</button>
+//             <button onClick={decrement}>Decrease</button>
+//             <button onClick={reset}>Reset</button>
+//         </div>
+
+//     )
+
+}
+
+export default Counter
